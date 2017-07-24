addf2b7e45b9dc80b6f0ff1632fe4a8ae9d0a63d
JaganNarayanan <jns405@nyu.edu>
2017-07-24 15:22:10 +0800

    Completed.

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
+var chosen;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+function randomRestaurant() {
+  restaurantArray = ['Pedas-pedas', 'Holy Chow', '7/11', 'Mamakan']
+  arrayLength = restaurantArray.length
+  rand = getRandomInt(0, arrayLength);
+  console.log(rand);
+
+  var colorArray = ["black","blue","red","#1d23f4","orange","#123123"]
+  var randomNumber = Math.floor((Math.random() * colorArray.length))
+
+  document.getElementById('restaurant').style.color = colorArray[randomNumber] ;
+  chosen = restaurantArray[rand];
+  restaurant.innerHTML = chosen
+}
+
+
 function addRestaurantListener(){
+
+  var myVar
+
+  function getRandRestaurant() {
+    myVar = setInterval(function() {randomRestaurant()}, 100);
+  }
+
   startBtn = document.getElementById('start-random-button');
-  
+
   // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
+  startBtn.addEventListener("click", getRandRestaurant)
+
 
   getBtn = document.getElementById('get-restaurant-button');
-  
+
   // Replace 'functionName' with the name of the function that you have written
-  getBtn.addEventListener("click", functionName);
+  text = document.getElementById('remark');
+
+  getBtn.addEventListener("click",  function () {
+      clearInterval(myVar)
+      text.innerHTML = "Great, let's go to " + chosen + "!"
+
+  });
+
+
+
 }
 
 // wait for DOM content to load before binding an event
---
+++ index2.js
@@ -0,0 +1,14 @@
+function addRestaurantListener(){
+  startBtn = document.getElementById('start-random-button');
+
+  // Replace 'functionName' with the name of the function that you have written
+  startBtn.addEventListener("click", functionName)
+
+  getBtn = document.getElementById('get-restaurant-button');
+
+  // Replace 'functionName' with the name of the function that you have written
+  getBtn.addEventListener("click", functionName);
+}
+
+// wait for DOM content to load before binding an event
+document.addEventListener('DOMContentLoaded', addRestaurantListener);
